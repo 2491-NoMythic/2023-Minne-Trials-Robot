c959253797e5c229ab9b9d112584d22cfea4e427
Xiaohan2491 <Xiaohan@dieterich.org>
2023-11-02 20:15:05 -0500

    Added Throttle for speed control

--- src/main/java/frc/robot/commands/DriveRobot.java
+++ src/main/java/frc/robot/commands/DriveRobot.java
@@ -1,8 +1,10 @@
 package frc.robot.commands;
 
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /** An example command that uses an example subsystem. */
 public class DriveRobot extends CommandBase {
@@ -30,10 +32,14 @@ public class DriveRobot extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-m_robotDrive.drive(m_robotsJoystick.getX(), m_robotsJoystick.getZ());
+    double throttle = m_robotsJoystick.getThrottle();
+m_robotDrive.drive(m_robotsJoystick.getZ() * 0.75, m_robotsJoystick.getY() * (throttle +2));
+SmartDashboard.putNumber("Throttle Value", m_robotsJoystick.getThrottle());
+SmartDashboard.putNumber("Joystick Input speed", m_robotsJoystick.getX());
+SmartDashboard.putNumber("Joystick Input Rotation", m_robotsJoystick.getZ());
   }
 
-
+ 
 
   // Called once the command ends or is interrupted.
   @Override
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -9,18 +9,19 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
-
 public class Drivetrain extends SubsystemBase {
   /** Creates a new Drivetrain. */
   private final WPI_TalonFX m_leftDrive;
   private final WPI_TalonFX m_rightDrive;
   private final DifferentialDrive m_robotDrive;
+
 public Drivetrain() {
     m_leftDrive = new WPI_TalonFX(1);
     m_rightDrive = new WPI_TalonFX(2);
     m_robotDrive = new DifferentialDrive(m_leftDrive, m_rightDrive);
     m_rightDrive.setNeutralMode(NeutralMode.Brake);
     m_leftDrive.setNeutralMode(NeutralMode.Brake); 
+    
   } 
 
 
